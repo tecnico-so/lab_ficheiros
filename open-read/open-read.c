b446ae2911d106a23a2bf9bef2ac5271684cc6fa
Ricardo Antunes <me@riscadoa.com>
2023-11-20 17:14:31 +0000

    fix: open-read.c should continue reading until EOF

--- open-read/open-read.c
+++ open-read/open-read.c
@@ -32,10 +32,19 @@ int main(int argc, char *argv[])
    memset(buffer,0,sizeof(buffer));
 
    /* read the contents of the file */
-   int bytes_read = read(fd, buffer, sizeof(buffer));
-   if (bytes_read < 0){
-      fprintf(stderr, "read error: %s\n", strerror(errno));
-      return -1;
+   int done = 0;
+   while (done < sizeof(buffer) - 1){
+      int bytes_read = read(fd, buffer + done, sizeof(buffer) - 1 - done);
+      if (bytes_read < 0){
+         fprintf(stderr, "read error: %s\n", strerror(errno));
+         return -1;
+      }
+
+      /* if we read 0 bytes, we're done */
+      if (bytes_read == 0)
+         break;
+
+      done += bytes_read;
    }
 
    printf("%s",buffer);
